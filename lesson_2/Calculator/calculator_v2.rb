c8184d3c1bc8ffce2058a9f26dbc4d0d975dc48a
fbraza <faouzi.brazza@gmail.com>
2018-12-21 22:08:47 +0000

    'Update lesson_2"

--- lesson_2/Calculator/calculator_v2.rb
+++ lesson_2/Calculator/calculator_v2.rb
@@ -6,35 +6,58 @@
 # answer = Kernel.gets()
 # Kernel.puts(answer)
 
+require 'yaml'
+MESSAGES = YAML.load_file('calculator_messages.yml')
+
 def prompt(message)
   puts("=> #{message}")
 end
 
+def integer?(num)
+  num.to_i.to_s == num
+end
+
+def float?(num)
+  num.to_f.to_s == num
+end
+
 def valid_number?(num)
-  num.to_i.to_s == num || num.to_f.to_s == num
+  float?(num) || integer?(num)
 end
 
-def operation_to_message(op)
-  word = case op
-         when '1'
-           'Adding'
-         when '2'
-           'Substracting'
-         when '3'
-           'Multiplying'
-         when '4'
-            'Dividing'
-         end
+def number_input?(num1, num2)
+  integer?(num1) && integer?(num2) 
+end
+# This method will allow to make choice toward the user input. 
+# if both numbers are integers then do the calculation accodingly.
+# if one of them is floats result will be display as float
+
+def operation_to_message(operator)
+  case operator
+  when '1' then 'Adding'
+  when '2' then 'Substracting'
+  when '3' then 'Multiplying'
+  when '4' then 'Dividing'
+  end
 end
 
-prompt("Welcome to Calculator! Please enter your name:")
+def calculation(num1, num2, operator)
+  case operator
+  when '1' then num1 + num2
+  when '2' then num1 - num2
+  when '3' then num1 * num2
+  when '4' then num1 / num2
+  end
+end
+
+prompt(MESSAGES['welcome'])
 
 name = ''
 loop do
   name = gets.chomp
 
   if name.empty?
-    prompt("Mak sure to use a valid name.")
+    prompt(MESSAGES['name'])
   else
     break
   end
@@ -45,25 +68,25 @@ prompt("Hello #{name}")
 loop do # main loop
   number1 = ''
   loop do
-    prompt("What is the first number?")
+    prompt(MESSAGES['number1'])
     number1 = gets().chomp()
 
     if valid_number?(number1)
       break
     else
-      prompt("ERROR: Please enter a valid number")
+      prompt(MESSAGES['number_error'])
     end
   end
 
   number2 = ''
   loop do
-    prompt("What is the second number?")
+    prompt(MESSAGES['number2'])
     number2 = gets().chomp()
 
     if valid_number?(number2)
       break
     else
-      prompt("ERROR: Please enter a valid number")
+      prompt(MESSAGES['number_error'])
     end
   end
 
@@ -84,28 +107,27 @@ loop do # main loop
     if ["1", "2", "3", "4"].include?(operator) # possibility to use %w(1 2 3 4)
       break
     else
-      prompt("ERROR: Make sure to enter choice 1, 2, 3 or 4")
+      prompt(MESSAGES['operator_error'])
     end
   end
 
   prompt("#{operation_to_message(operator)} the two numbers...")
 
-  result = case operator
-           when '1'
-             number1.to_i + number2.to_i
-           when '2'
-             number1.to_i - number2.to_i
-           when '3'
-             number1.to_i * number2.to_i
-           when '4'
-             number1.to_f / number2.to_f
-           end
+  if number_input?(number1, number2)
+    number1 = number1.to_i
+    number2 = number2.to_i
+  else
+    number1 = number1.to_f
+    number2 = number2.to_f
+  end
+
+  result = calculation(number1, number2, operator)
 
   prompt("The results is: #{result}")
 
-  prompt("Do you want to perform another opeartion? (Y or N)")
+  prompt(MESSAGES['another_operation'])
   answer = gets().chomp()
   break unless answer.downcase == 'y'
 end
 
-prompt("Thank you for using our calculator program!")
+prompt(MESSAGES['goodbye'])
